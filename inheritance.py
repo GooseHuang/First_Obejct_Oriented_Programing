2e269a41c4cfffaf8bf5fef613e03b0cfdd65934
huangqiliang <goosehuangmatt@gmail.com>
2021-01-14 21:38:43 +0800

    feat:Parent and Child

--- inheritance.py
+++ inheritance.py
@@ -4,11 +4,23 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name -", self.last_name)
+        print("Eye Color -", self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child Constructer Called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
+
+    def show_info(self):
+        print("Last Name -", self.last_name)
+        print("Eye Color -", self.eye_color)
+        print("Number of Toys -", self.number_of_toys)
+
+
+
 
 billy_cyrus = Parent("Cyrus","blue")
 print(billy_cyrus.last_name)
@@ -16,3 +28,4 @@ print(billy_cyrus.last_name)
 goose_cyrus = Child("Cyrus","black","90")
 print(goose_cyrus.last_name)
 print(goose_cyrus.number_of_toys)
+goose_cyrus.show_info()
